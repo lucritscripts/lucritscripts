e462531ab34548c9cb294c219ce4cb4911e1656f
lucritscripts <lucritscripts@gmail.com>
2026-08-15 23:01:48 -0500

    Delete functions/index.js

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-// Entry point. Firebase discovers exported functions from this file.
-
-import { initializeApp } from "firebase-admin/app";
-
-initializeApp();
-
-export { assistant } from "./assistant.js";
-export { unlocks } from "./unlocks.js";
